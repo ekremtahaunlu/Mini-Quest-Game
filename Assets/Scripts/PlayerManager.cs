1a4e1c76d55f2dc01768f3cf7a3aee7e0ee56ee0
Ekrem Taha Ünlü <ekremtahaunlu@users.noreply.github.com>
2024-10-29 18:59:43 +0300

    Materials created.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,21 +1,33 @@
-using System.IO;
-using Photon.Pun;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+using System.IO;
 
 public class PlayerManager : MonoBehaviour
 {
-    private PhotonView _pv;
+	private PhotonView _pv;
 
-    private void Start()
-    {
-        if (_pv.IsMine)
-        {
-            CreateController();
-        }
-    }
+	private void Awake()
+	{
+		_pv = GetComponent<PhotonView>();
+	}
 
-    private void CreateController()
-    {
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs","PlayerController"), Vector3.zero, Quaternion.identity);
-    }
+	private void Start()
+	{
+		if(_pv.IsMine)
+		{
+			CreateController();
+		}
+	}
+
+	private static void CreateController()
+	{
+		PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
+	}
+
+	public static PlayerManager Find(Player player)
+	{
+		return FindObjectsOfType<PlayerManager>().SingleOrDefault(x => Equals(x._pv.Owner, player));
+	}
 }
